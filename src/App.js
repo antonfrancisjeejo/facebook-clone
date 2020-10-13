7b8443d060a5252a299cd720f8791db972127d70
unknown <anton13jeejo@gmail.com>
2020-10-13 20:36:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { useStateValue } from "./components/Data/StateProvider";
+import Feed from "./components/Feed/Feed";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Widgets from "./components/Widgets/Widgets";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  return (
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;

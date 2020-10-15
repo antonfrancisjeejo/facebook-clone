e21cb567669e5229f8bde0672a1e439be5b130f1
unknown <anton13jeejo@gmail.com>
2020-10-15 20:03:22 +0530

    Initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,40 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import { auth, provider } from "../../firebase";
+import { actionTypes } from "../Data/reducer";
+import { useStateValue } from "../Data/StateProvider";
+import "./Login.css";
+
+const Login = () => {
+  const [state, dispatch] = useStateValue();
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((err) => alert(err.message));
+  };
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
+          alt=""
+        />
+        <img
+          src="https://download.logo.wine/logo/Facebook/Facebook-Logo.wine.png"
+          alt=""
+        />
+      </div>
+      <Button type="submit" onClick={signIn}>
+        Sign In
+      </Button>
+    </div>
+  );
+};
+
+export default Login;
